808cef45d5517c244e34204111457622fb7b0d41
klinovsky <sebastian.klinovsky@student.uclouvain.be>
2023-07-27 12:56:08 +0200

    Almost final version of Grape Bot volunteer

---
+++ email_sender.py
@@ -0,0 +1,19 @@
+# import smtplib
+# from email.message import EmailMessage
+# import smtplib
+# from getpass import getpass
+
+# # print("sending email ...")
+
+# # msg = EmailMessage()
+# # msg['Subject'] = 'GrapeFestival'
+# # msg['From'] = 'Sebastian Klinovsky'
+# # msg['To'] = 'slavka.kurjak@gmail.com'
+# # msg.set_content('GrapeFestival - open positions 2')
+
+# # myServer = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+# # myServer.login('sebastian.klin@gmail.com', 'jrnmpoyjtjaicylr')
+# # myServer.send_message(msg)
+# # myServer.quit()
+
+
